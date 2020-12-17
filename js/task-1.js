5370ec9893464d6b2a286fb7d05ef694d8da9d5e
Natalia2525 <65954006+Natalia2525@users.noreply.github.com>
2020-12-17 18:54:50 +0200

    выполнено

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,12 @@
-// Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд. 
+// Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд.
 // Значением исполнившегося промиса должно быть то кол - во миллисекунд которое передали во время вызова функции delay.
 
 const delay = ms => {
-  
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
 };
 
 const logger = time => console.log(`Resolved after ${time}ms`);
